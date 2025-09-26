88c2e48db5d678a65922dd30454cfefd8d0d428a
Sushmitha5972 <my7lenovomail@gmail.com>
2025-09-26 15:37:54 +0530

    Intial commit

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config();  // must be at top
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
